79892d789f6a85a27eabb4e804cec562e5de81dd
kaminaka rikuya <rikupin1105@gmail.com>
2021-11-22 17:02:41 +0900

    Create ILineNotifyClient.cs

---
+++ LineNotifySharp/ILineNotifyClient.cs
@@ -0,0 +1,12 @@
+﻿using LineNotifySharp.Model;
+using System.Threading.Tasks;
+
+namespace LineNotifySharp;
+public interface ILineNotifyClient
+{
+    Task SendMessageAsync(MessageObject messageObject);
+    Task<bool> IsValidAccessTokenAsync();
+    Task<bool> RevokeAsync();
+    Task<string> GetAccessTokenAsync(string code, string redirectUri, string clientId, string clientSecret);
+    Task SendTextMessageAsync(string text);
+}
