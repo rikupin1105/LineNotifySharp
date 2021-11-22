63aa31f51d2c05d83fdd49fff0dd2889d3f84817
kaminaka rikuya <rikupin1105@gmail.com>
2021-11-22 16:12:44 +0900

    Create AccessTokenObject .cs

---
+++ src/LineNotifySharp/Model/AccessTokenObject .cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace LineNotifySharp.Model
+{
+    class AccessTokenObject
+    {
+        public AccessTokenObject(string accessToken)
+        {
+            AccessToken = accessToken ?? throw new ArgumentNullException(nameof(accessToken));
+        }
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+    }
+}
